351946eccf8d1cd21e8b762ea3b2b743879930c9
jitendrakhilar <jitendra.khilar@akqa.com>
2023-12-16 13:37:50 +0530

    feat: grid scss added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,22 @@
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="mtb_container">
+      <div> LOGO </div>
+      <nav>
+        <div> HOME </div>
+        <div> PRODUCTS </div>
+        <div> CATEGORIES </div>
+        <div>ABOUT</div>
+        <div>CONTACT US</div>
+      </nav>
+
+      <div>
+        <div>Search</div>
+        <div>Add to Bag</div>
+        <div>My profile</div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
