91722ede84fb072696acc97b754b8214f924fe6e
jitendrakhilar <jitendra.khilar@akqa.com>
2023-12-23 10:45:20 +0530

    feat: slider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import styles from './page.module.css';
 
-import SliderSlick from '@/components/slick/slider';
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <SliderSlick />
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
